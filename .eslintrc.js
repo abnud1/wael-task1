b2ec46e1326c85ee465095be24fe8df80158a66a
abnud1 <ahmad13932013@hotmail.com>
2021-02-09 18:42:30 +0200

    format eslint config and configure some aria rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
-    extends: [
-        'airbnb-typescript',
-        "prettier",
-        "prettier/@typescript-eslint",
-        "prettier/babel",
-        "prettier/prettier",
-        "prettier/react",
+  extends: [
+    "airbnb-typescript",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/babel",
+    "prettier/prettier",
+    "prettier/react",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/label-has-associated-control": [
+      "error",
+      {
+        required: {
+          some: ["nesting", "id"],
+        },
+      },
     ],
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    rules: {
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off"
-    }
+    "jsx-a11y/label-has-for": [
+      "error",
+      {
+        required: {
+          some: ["nesting", "id"],
+        },
+      },
+    ],
+  },
 };
